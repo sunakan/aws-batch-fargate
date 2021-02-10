6d769794c8d88f6a8d15175df39c7c68729bddc2
sunakan <sunakan03@gmail.com>
2021-02-10 22:03:01 +0900

    job-app-01

---
+++ job-app-01/main.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e
+
+################################################################################
+# $ sh main.sh $(date -d '1 month ago' +'%Y-%m'-01) $(date '+%Y-%m'-01)
+################################################################################
+
+readonly LAST_MONTH_DAY_1=$1 #`date -d '1 month ago' +'%Y-%m'`-01
+readonly CURRENT_MONTH_DAY_1=$2 #`date '+%Y-%m'`-01
+
+echo 開始：${LAST_MONTH_DAY_1}
+echo 終了：${CURRENT_MONTH_DAY_1}
+
+#aws ce get-dimension-values --time-period Start=${LAST_MONTH_DAY_1},End=${CURRENT_MONTH_DAY_1} --dimension SERVICE --context COST_AND_USAGE \
+
+echo 'サービス,使用量(単位:不明),割引前料金,割引後料金'
+aws ce get-cost-and-usage \
+  --region ap-northeast-1 \
+  --time-period Start=${LAST_MONTH_DAY_1},End=${CURRENT_MONTH_DAY_1} \
+  --granularity MONTHLY \
+  --metrics "UnblendedCost" "BlendedCost" "UsageQuantity" \
+  --group-by Type=DIMENSION,Key=SERVICE \
+| jq -rc '.ResultsByTime[].Groups[] | [.Keys[], .Metrics.UsageQuantity.Amount, .Metrics.UnblendedCost.Amount, .Metrics.BlendedCost.Amount ] | @csv' \
+| sed -e 's/"//g'
